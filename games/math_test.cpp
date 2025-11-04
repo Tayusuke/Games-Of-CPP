357caaf095f3dfe16bb72e37456cd2b0e865aae7
tayuta <tayutyman@hotmail.com>
2025-11-04 12:29:33 +0900

    Improve randomness in math_test.cpp
    Replaced srand and rand with random_device and uniform_int_distribution for better randomness.

--- games/math_test.cpp
+++ games/math_test.cpp
@@ -1,15 +1,17 @@
 #include <iostream>
-#include <cstdlib>
-#include <ctime>
 #include <string>
+#include <random>
 
 using namespace std;
 
 int main() {
 	string your_answer = "";
-	srand(time(nullptr));
-	int number_a = rand() % 10000 + 1;
-	int number_b = rand() % 10000 + 1;
+	random_device rd;
+
+	std::uniform_int_distribution<int> dist(1, 10000);
+
+	int number_a = dist(rd);
+	int number_b = dist(rd);
 
 	int the_answer = number_a + number_b;
 
@@ -22,6 +24,7 @@ int main() {
 
 		cout << number_a << " + " << number_b << " = ?" << endl;
 		cin >> your_answer;
+		isFirst = false;
 	}
 
 	cout << "Correct!!!" << endl;
