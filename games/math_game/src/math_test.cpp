bd99213179a248d0c8f0ce60dfbfb2606b5225a4
tayuta <tayutyman@hotmail.com>
2025-11-06 18:41:13 +0900

    Refactor math_test.cpp by cleaning up code
    Removed unnecessary includes and simplified the main function.

--- games/math_game/src/math_test.cpp
+++ games/math_game/src/math_test.cpp
@@ -1,8 +1,5 @@
-#include <iostream>
-#include <string>
+#include "math.h"
 #include <random>
-
-using namespace std;
 
 void generate_Random(int& a, int& b, int& ans) {
 	random_device rd;
@@ -13,35 +10,4 @@ void generate_Random(int& a, int& b, int& ans) {
 	b = dist(rd);
 
 	ans = a + b;
-}
-
-int main() {
-	string your_answer = "";
-
-	int number_a, number_b, the_answer;
-
-	generate_Random(number_a, number_b, the_answer);
-
-	bool isFirst = true;
-
-	while (!(your_answer == "y")) {
-		while (your_answer != to_string(the_answer)) {
-			if (!isFirst) {
-				cout << "Incorrect..." << endl << endl;
-			}
-
-			cout << number_a << " + " << number_b << " = ?" << endl << endl;
-			cin >> your_answer;
-			cout << endl;
-			isFirst = false;
-		}
-
-		cout << "Correct!!!" << endl << endl;
-		isFirst = true;
-
-		generate_Random(number_a, number_b, the_answer);
-		cout << "Do you want to exit this test? y/n" << endl << endl;
-		cin >> your_answer;
-		cout << endl;
-	}
 }
